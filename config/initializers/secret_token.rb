4d75363f75b8c45692e413e7810bf9982e1068a2
mzdravkov <mihail0zdravkov@gmail.com>
2013-10-25 20:22:36 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AoCallendar::Application.config.secret_key_base = '65cdeb6d7263f51ab5a56c504c311f51d29cf7fe2f4e8da09d08f37a80422efd7e126899c09bf9dbbbd9f7cd508e92dad3e3e78769261b90fdab6bf6f6ed2988'
