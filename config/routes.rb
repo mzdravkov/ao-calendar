d77569b54178f581ad8e98ed58c1f7400aa47e02
mzdravkov <mihail0zdravkov@gmail.com>
2013-10-26 05:07:07 +0300

    Fix dodo's failed merging

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 AoCallendar::Application.routes.draw do
-  resources :events
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
-<<<<<<< HEAD
-=======
+  resources :events
+
   resources :group_calendars, except: :index
->>>>>>> d984998599f9ca95b2f5c75d3a027310c4ab6f7c
 end
